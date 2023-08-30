5c8a11d612e34641c5eab52776377445469ad82b
elonehoo <hi@elonehoo.me>
2023-08-30 20:57:04 +0800

    refactor: move to `unplugin`

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface Options {
+  /**
+   * Git repo root path
+   */
+  root?: string
+
+  /**
+   * Github repo url
+   */
+  github?: string
+
+  /**
+   * Custom virtual module prefix
+   *
+   * @default '~build'
+   */
+  prefix?: string
+
+  /**
+   * Pass some meta data to Vite app
+   *
+   * Notice: meta data will be serialized to JSON format
+   */
+  meta?: Record<string | number | symbol, any>
+}
