a51a9e1ec63591dac55d92e69ba0d75f3bcb2dcc
XLor <yjl9903@vip.qq.com>
2023-09-25 20:36:54 +0800

    feat: add astro plugin

---
+++ src/astro.ts
@@ -0,0 +1,13 @@
+import type { Options } from './core';
+
+import VitePlugin from './vite';
+
+export default (options: Options | undefined) => ({
+  name: 'unplugin-info',
+  hooks: {
+    'astro:config:setup': async (astro: any) => {
+      astro.config.vite.plugins ||= [];
+      astro.config.vite.plugins.push(VitePlugin(options));
+    }
+  }
+});
