debae5fb12bee5e82100e63b4f8f6fc1fc7925e0
lawvs <18554747+lawvs@users.noreply.github.com>
2024-01-26 04:10:23 +0800

    fix: deploy base

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import BuildInfo from 'unplugin-info/vite';
 
 export default defineConfig({
+  base: './',
   plugins: [BuildInfo({ meta: { message: 'This is set from vite.config.ts' } })]
 });
