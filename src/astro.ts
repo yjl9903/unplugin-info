ecafc08e2a7b7c3b71f4f5e46a4246ada77cf686
XLor <yjl9903@vip.qq.com>
2024-03-20 15:10:25 +0800

    fix: astro default options

--- src/astro.ts
+++ src/astro.ts
@@ -2,7 +2,7 @@ import type { Options } from './core';
 
 import VitePlugin from './vite';
 
-export default (options: Options | undefined) => ({
+export default (options: Options = {}) => ({
   name: 'unplugin-info',
   hooks: {
     'astro:config:setup': async (astro: any) => {
